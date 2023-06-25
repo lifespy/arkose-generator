5e36f786701d9bb23c62f2d6159107406387b0bf
Antonio <acheong@student.dalat.org>
2023-06-26 02:02:08 +0800

    API

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-const fun = require("./api");
-async function main() {
-  const token = await fun.getToken({
-    pkey: "35536E1E-65B4-4D96-9D97-6ADB7EFF8147", // The public key
-    surl: "https://tcr9i.chat.openai.com ", // OPTIONAL: Some websites can have a custom service URL
-    headers: {
-      // OPTIONAL
-      // You can pass custom headers if you have to, but keep
-      // in mind to pass a user agent when doing that
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
-    },
-    site: "https://chat.openai.com", // OPTIONAL: The site parameter, usually not required
-  });
-  console.log(token);
-}
-main();
+const express = require('express');
+const fun = require('./api');
+
+const app = express();
+const port = 3000; // You can change the port number as needed
+
+app.get('/api/arkose', async (req, res) => {
+  try {
+    const token = await fun.getToken({
+      pkey: '35536E1E-65B4-4D96-9D97-6ADB7EFF8147',
+      surl: 'https://tcr9i.chat.openai.com',
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+      },
+      site: 'https://chat.openai.com',
+    });
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to retrieve token' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API server listening on port ${port}`);
+});
